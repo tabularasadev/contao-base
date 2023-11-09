462105e7072b1583d5a7c5171f0e31a746f00f64
Jimmy <43845748+tabularasadev@users.noreply.github.com>
2023-11-09 16:43:11 +0100

    Update ceBase.php

--- src/Element/ceBase.php
+++ src/Element/ceBase.php
@@ -4,6 +4,10 @@ namespace trdev\ContaoBaseBundle\Element;
 
 class ceBase extends \ContentElement
 {
+    //Javascript et CSS additionnels a l'élément
+    private $javascripts = [];
+    private $styles = [];
+    
     protected $strTemplate = "ce_base";
 
     public function generate()
@@ -19,6 +23,18 @@ class ceBase extends \ContentElement
             $objTemplate->href     = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
 
             return $objTemplate->parse();
+        } else {
+            if (Count($this->javascripts) > 0) {
+                foreach ($this->javascripts as $js) {
+                    $GLOBALS['TL_JAVASCRIPT'][] = $js;
+                }
+            }
+
+            if (Count($this->styles) > 0) {
+                foreach ($this->styles as $css) {
+                    $GLOBALS['TL_CSS'][] = $css;
+                }
+            }
         }
 
         if (!isset($_GET['items']) && \Config::get('useAutoItem') && isset($_GET['auto_item'])) {
